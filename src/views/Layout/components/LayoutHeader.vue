8dd79ba76869abf11e280767e66f7f48330030ec
zs <3199441913@qq.com>
2026-04-06 13:24:19 +0800

    layout-一级导航渲染完成

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,17 @@
 <script setup>
+  import { getCategoryAPI } from "@/apis/layout";
+  import { onMounted } from "vue";
+  import { ref } from "vue";
 
+  const getCateList = ref([]);
+  const getCategory = async () => {
+    const res = await getCategoryAPI();
+    getCateList.value = res.data.result;
+  }
+
+  onMounted(() => {
+    getCategory()
+  })
 </script>
 
 <template>
@@ -9,12 +21,9 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+        <li class="home" v-for="item in getCateList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
