67aa7c12d8bd32678b09b9729613f35ffbd0aef1
zs <3199441913@qq.com>
2026-04-07 15:50:08 +0800

    二级路由的分类配置以及完善导航显示问题

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -30,7 +30,7 @@ const { getCategory } = useCategory()
         <h3>全部分类</h3>
         <ul>
           <li v-for="i in getCategory.children" :key="i.id">
-            <RouterLink to="/">
+            <RouterLink :to="`/category/sub/${i.id}`">
               <img :src="i.picture" />
               <p>{{ i.name }}</p>
             </RouterLink>
--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -15,7 +15,7 @@
       <!-- 导航区域 -->
       <ul class="app-header-nav ">
         <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+          <RouterLink exact-active-class="active" to="/">首页</RouterLink>
         </li>
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink active-class="active" :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
