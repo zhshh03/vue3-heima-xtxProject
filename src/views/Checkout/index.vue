feebd04cb439d239e4c8cfa4ec8aa16e08e91e9b
zs <3199441913@qq.com>
2026-04-10 22:18:32 +0800

    切换地址-显示弹框及基本数据渲染

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -12,10 +12,12 @@ const getCheckoutInfo = async() => {
   const item = checkInfo.value?.userAddresses.find(item => item.isDefault === 1 )
   curAddress.value = item
 }
+const showDialog = ref(false)
 
 onMounted(() => {
   getCheckoutInfo()
 })
+
 </script>
 
 <template>
@@ -35,7 +37,7 @@ onMounted(() => {
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true">切换地址</el-button>
+              <el-button size="large" @click="showDialog = true">切换地址</el-button>
               <el-button size="large" @click="addFlag = true">添加地址</el-button>
             </div>
           </div>
@@ -116,10 +118,28 @@ onMounted(() => {
     </div>
   </div>
   <!-- 切换地址 -->
+   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+        <ul>
+        <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+        <li><span>联系方式：</span>{{ item.contact }}</li>
+        <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
 <style scoped lang="scss">
+@use "sass:color";
 .xtx-pay-checkout-page {
   margin-top: 20px;
 
@@ -321,7 +341,7 @@ onMounted(() => {
     &.active,
     &:hover {
       border-color: $xtxColor;
-      background: lighten($xtxColor, 50%);
+      background: color.scale($xtxColor, $lightness: 89%);
     }
 
     >ul {
